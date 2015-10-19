ebaafa22df500bf65ddbed4aab1f01b7e79e0a4b
Speaslee <sophiapeaslee@gmail.com>
2015-10-19 18:47:07 -0400

    made search work

--- console.rb
+++ console.rb
@@ -1,4 +1,7 @@
 require "./db/setup"
 require "./lib/all"
 
+require "./song_search.rb"
+
+Search.find_song_spotify "stay with me"
 binding.pry
--- song_search.rb
+++ song_search.rb
@@ -1,24 +1,23 @@
 require 'httparty'
 class Search
-attr_reader :song
-
-def initalize data_hash
-  @title = data_hash["name"]
-  @artist = data_hash["artists"]
-  @album = data_hash["album"]
-  @preview_url = data_hash["preview_url"]
-
-end
-
-def find_song_spotify song
-  s = song.gsub(/\s/,'+')
-  r = HTTParty.get("https://api.spotify.com/v1/search?q=#{s}&type=track")
-  r["tracks"].map {h Search.new(h)}
-  "#{h["name"]} by #{}"
-end
-
-def results
+  attr_reader :title, :album_name, :album_image, :preview_url, :artist
 
 
+  def self.find_song_spotify song
 
+    s = song.gsub(/\s/,'+')
+    key = File.read "./api.txt"
+    r = HTTParty.get "https://api.spotify.com/v1/search?q=#{s}&type=track",
+    headers:{"Authorization" => "Bearer #{key}"}
+    r["tracks"]["items"].each do |song|
+
+      @title = song["name"]
+      @album_name =  song["album"]["name"]
+      @album_image = song["album"]["images"][1]
+      @artist = song["artists"][0]["name"]
+      @preview_url = song["preview_url"]
+      binding.pry
+    end
+  end
+  puts "#{@title}, by #{@artist}"
 end
